3804c974187d96f5dc477e0d1dd8ec07ec04246a
Adil7 <adil.mahmood@ryerson.ca>
2020-06-07 22:51:41 -0400

    first commit

---
+++ SlidingWindow/src/LongestSubarrayWithOnes.java
@@ -0,0 +1,9 @@
+public class LongestSubarrayWithOnes {
+    public static int findLength(int[] arr, int k){
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(findLength(new int[] {0,0,0,1,1}, 2));
+    }
+}
